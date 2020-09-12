1418960f240c771e084a8b50c8bc86a47b7cd699
kspar <kaspar.papli@gmail.com>
2020-09-12 21:51:39 +0200

    conf from root object constructor instead of file, refactor auth to RequestUtil

--- ez.py
+++ ez.py
@@ -1,31 +1,47 @@
 import dataclasses
 import json
 import logging
+import socket
+import threading
 import time
 import typing as T
+import webbrowser
 from dataclasses import dataclass
 
 import requests
+from flask import Flask, request, Response, render_template
 
-import auth
-import conf
 import data
 import util
 from defaults import read_token, write_tokens
 
 
 # TODO:
-# conf
+# hide private fields/methods
+
 
 class RequestUtil:
     def __init__(self,
+                 api_url: str,
+                 idp_url: str,
+                 idp_client_name: str,
+                 auth_token_min_valid_sec: int,
+                 auth_port_range_first: int,
+                 auth_port_range_last: int,
                  retrieve_tokens: T.Callable[[data.Token], T.Tuple[str, int]],
                  persist_tokens: T.Callable[[str, int, str], None]):
+
+        self.api_url = api_url
+        self.idp_url = idp_url
+        self.idp_client_name = idp_client_name
+        self.auth_token_min_valid_sec = auth_token_min_valid_sec
+        self.auth_port_range_first = auth_port_range_first
+        self.auth_port_range_last = auth_port_range_last
         self.retrieve_tokens = retrieve_tokens
         self.persist_tokens = persist_tokens
 
     def simple_get_request(self, path: str, response_dto_class: T.Type[T.Any]) -> T.Any:
-        resp: requests.Response = requests.get(path, headers=self.get_token_header())
+        resp: requests.Response = requests.get(self.api_url + path, headers=self.get_token_header())
         dto = util.handle_response(resp, {200: response_dto_class})
         assert isinstance(dto, response_dto_class)
         return dto
@@ -33,27 +49,111 @@ class RequestUtil:
     # TODO. can also return DTO from this
     def post_request(self, path: str, request_dto_dataclass: T.Any) -> int:
         json_data = json.dumps(dataclasses.asdict(request_dto_dataclass)).encode("utf-8")
-        resp: requests.Response = requests.post(path, data=json_data, headers=self.get_token_header())
+        resp: requests.Response = requests.post(self.api_url + path, data=json_data, headers=self.get_token_header())
         return resp.status_code
 
     def get_token_header(self) -> T.Dict[str, str]:
         access_token_file, expires_at = self.retrieve_tokens(data.Token.ACCESS)
 
         # TODO: make sure and test that we account for clock skew
-        if access_token_file is None or time.time() > expires_at + conf.AUTH_TOKEN_MIN_VALID_SEC:
-            auth.auth(self.retrieve_tokens, self.persist_tokens)
+        if access_token_file is None or time.time() > expires_at + self.auth_token_min_valid_sec:
+            self.auth()
             access_token_file, expires_at = self.retrieve_tokens(data.Token.ACCESS)
 
-            if access_token_file is None or time.time() > expires_at + conf.AUTH_TOKEN_MIN_VALID_SEC:
+            if access_token_file is None or time.time() > expires_at + self.auth_token_min_valid_sec:
                 raise RuntimeError("Could not get/refresh tokens")
 
         return {"Authorization": f"Bearer {access_token_file[data.Token.ACCESS.value]}"}
 
+    def auth(self):
+        app = Flask(__name__)
+
+        def shutdown_server():
+            func = request.environ.get('werkzeug.server.shutdown')
+            if func is None:
+                raise RuntimeError('Not running with the Werkzeug Server')
+            func()
+
+        @app.route('/keycloak.json')
+        def controller_keycloak_conf():
+            return render_template("keycloak.json", idp_url=self.idp_url, client_name=self.idp_client_name)
+
+        @app.route('/login')
+        def controller_login():
+            return render_template("login.html", idp_url=self.idp_url, port=port)
+
+        @app.route('/deliver-tokens', methods=['POST'])
+        def controller_deliver_tokens():
+            try:
+                if request.is_json:
+                    body = request.get_json()
+                    self.persist_tokens(body[data.Token.ACCESS.value],
+                                        int(body['access_token_valid_sec']),
+                                        body[data.Token.REFRESH.value])
+
+                    return Response(status=200)
+                else:
+                    return Response(status=400)
+            finally:
+                shutdown_server()
+
+        if self._refresh_using_refresh_token():
+            return
+
+        local, port = "127.0.0.1", self._get_free_port()
+        url = f'http://{local}:{port}/login'
+        thread = threading.Thread(target=app.run, args=(local, port, False, False,))
+
+        # Assume the server starts in 1 second
+        threading.Timer(1, lambda: webbrowser.open(url)).start()
+
+        thread.start()
+        thread.join()
+
+    def _get_free_port(self) -> int:
+        for p in range(self.auth_port_range_first, self.auth_port_range_last + 1):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                sock.bind(('127.0.0.1', p))
+                sock.close()
+                return p
+            except OSError:
+                # Port already in use?
+                pass
+
+        raise OSError(f"Unable to bind to ports {self.auth_port_range_first} - {self.auth_port_range_last}")
+
+    def _refresh_using_refresh_token(self) -> bool:
+        refresh_token, _ = self.retrieve_tokens(data.Token.REFRESH)
+
+        if refresh_token is None:
+            logging.debug("No refresh token found")
+            return False
+
+        token_req_body = {
+            'grant_type': "refresh_token",
+            'refresh_token': refresh_token,
+            'client_id': self.idp_client_name
+        }
+
+        r = requests.post(f"{self.idp_url}/auth/realms/master/protocol/openid-connect/token", data=token_req_body)
+
+        if r.status_code == 200:
+            resp_body = r.json()
+            self.persist_tokens(resp_body[data.Token.ACCESS.value],
+                                resp_body['expires_in'],
+                                resp_body[data.Token.REFRESH.value])
+
+            logging.debug("Refreshed tokens using refresh token")
+            return True
+        else:
+            logging.warning(f"Refreshing tokens failed with status {r.status_code}")
+            return False
+
 
 class Student:
-    def __init__(self, root: str,
+    def __init__(self,
                  request_util: RequestUtil):
-        self.root: str = root
         self.request_util = request_util
 
     def get_courses(self) -> data.StudentCourseResp:
@@ -61,7 +161,7 @@ class Student:
         GET summaries of courses the authenticated student has access to.
         """
         logging.debug(f"GET summaries of courses the authenticated student has access to")
-        path = f"{self.root}/student/courses"
+        path = "/student/courses"
         return self.request_util.simple_get_request(path, data.StudentCourseResp)
 
     def get_exercise_details(self, course_id: str, course_exercise_id: str) -> data.ExerciseDetailsResp:
@@ -70,7 +170,7 @@ class Student:
         """
         logging.debug(f"GET exercise details for course '{course_id}' exercise '{course_exercise_id}'")
         util.assert_not_none(course_id, course_exercise_id)
-        path = f"{self.root}/student/courses/{course_id}/exercises/{course_exercise_id}"
+        path = f"/student/courses/{course_id}/exercises/{course_exercise_id}"
         return self.request_util.simple_get_request(path, data.ExerciseDetailsResp)
 
     def get_latest_exercise_submission_details(self, course_id: str, course_exercise_id: str) -> data.SubmissionResp:
@@ -79,7 +179,7 @@ class Student:
         """
         logging.debug(f"GET latest submission's details to the '{course_id}' exercise '{course_exercise_id}'")
         util.assert_not_none(course_id, course_exercise_id)
-        path = f"{self.root}/student/courses/{course_id}/exercises/{course_exercise_id}/submissions/latest/await"
+        path = f"/student/courses/{course_id}/exercises/{course_exercise_id}/submissions/latest/await"
         return self.request_util.simple_get_request(path, data.SubmissionResp)
 
     def get_all_submissions(self, course_id: str, course_exercise_id: str) -> data.StudentAllSubmissionsResp:
@@ -88,7 +188,7 @@ class Student:
         """
         logging.debug(f" GET submissions to course '{course_id}' course exercise '{course_exercise_id}'")
         util.assert_not_none(course_id, course_exercise_id)
-        path = f"{self.root}/student/courses/{course_id}/exercises/{course_exercise_id}/submissions/all"
+        path = f"/student/courses/{course_id}/exercises/{course_exercise_id}/submissions/all"
         return self.request_util.simple_get_request(path, data.StudentAllSubmissionsResp)
 
     def post_submission(self, course_id: str, course_exercise_id: str, solution: str) -> int:
@@ -102,14 +202,13 @@ class Student:
         class Submission:
             solution: str
 
-        path = f"{self.root}/student/courses/{course_id}/exercises/{course_exercise_id}/submissions"
+        path = f"/student/courses/{course_id}/exercises/{course_exercise_id}/submissions"
         return self.request_util.post_request(path, Submission(solution))
 
 
 class Teacher:
-    def __init__(self, root: str,
+    def __init__(self,
                  request_util: RequestUtil):
-        self.root: str = root
         self.request_util = request_util
 
     def get_courses(self) -> data.TeacherCourseResp:
@@ -117,24 +216,31 @@ class Teacher:
         GET summaries of courses the authenticated teacher has access to.
         """
         logging.debug(f"GET summaries of courses the authenticated teacher has access to")
-        path = f"{self.root}/teacher/courses"
+        path = "/teacher/courses"
         return self.request_util.simple_get_request(path, data.TeacherCourseResp)
 
 
+# TODO: token methods to optional
 class Ez:
     def __init__(self,
+                 api_base_url: str,
+                 idp_url: str,
+                 idp_client_name: str,
                  retrieve_tokens: T.Callable[[data.Token], T.Tuple[str, int]],
                  persist_tokens: T.Callable[[str, int, str], None],
+                 auth_token_min_valid_sec: int = 20,
+                 auth_port_range_first: int = 5100,
+                 auth_port_range_last: int = 5109,
                  logging_level: int = logging.INFO):
         """
-
-        :param retrieve_tokens:
-        :param persist_tokens:
+        TODO
         :param logging_level: default logging level, e.g. logging.DEBUG. Default: logging.INFO
         """
-        self.util = RequestUtil(retrieve_tokens, persist_tokens)
-        self.student: Student = Student(conf.BASE_URL, self.util)
-        self.teacher: Teacher = Teacher(conf.BASE_URL, self.util)
+        versioned_api_url = f'{api_base_url}/v2'
+        self.util = RequestUtil(versioned_api_url, idp_url, idp_client_name, auth_token_min_valid_sec,
+                                auth_port_range_first, auth_port_range_last, retrieve_tokens, persist_tokens)
+        self.student: Student = Student(self.util)
+        self.teacher: Teacher = Teacher(self.util)
         logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s : %(message)s', level=logging_level)
 
 
@@ -147,6 +253,7 @@ if __name__ == '__main__':
     # print(ez.student.post_submission("1", "1", "solution1"))
     # print(ez.teacher.get_courses())
 
-    ez = Ez(read_token, write_tokens)
+    ez = Ez('https://dev.ems.lahendus.ut.ee', 'https://dev.idp.lahendus.ut.ee', 'dev.lahendus.ut.ee', read_token,
+            write_tokens, logging_level=logging.DEBUG)
     print(ez.student.get_courses())
     # print(ez.student.get_exercise_details("2", "1"))
